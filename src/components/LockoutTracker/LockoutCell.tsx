db4a72228b9fc4ecc4f90166a55eefd299afd33b
Kevin Duff <kevinkelduff@gmail.com>
2022-03-19 17:45:19 +0000

    Add seen marker on pokemon you've seen

--- src/components/LockoutTracker/LockoutCell.tsx
+++ src/components/LockoutTracker/LockoutCell.tsx
@@ -1,9 +1,12 @@
 import usePlayersList from "../../hooks/usePlayersList";
 import usePokemonInfo from "../../hooks/usePokemonInfo";
+import usePokemonSeen from "../../hooks/usePokemonSeen";
+import useUser from "../../hooks/useUser";
 import { GameEvent } from "../../types";
 import { useStyletron } from "baseui";
 import { StatefulTooltip } from "baseui/tooltip";
 import moment from "moment";
+import { FaEye } from "react-icons/fa";
 
 const LockoutCell = ({
   name,
@@ -19,26 +22,46 @@ const LockoutCell = ({
   const [{ data }] = usePokemonInfo({ num });
   const [css] = useStyletron();
 
+  const { user } = useUser();
+  const seen = usePokemonSeen({
+    gameId,
+    playerId: user?.uid ?? "",
+    pokemonName: name,
+  });
+
   const { players } = usePlayersList({ gameId });
-  const player = players.find((player) => player.uid === ownEvent?.player_id);
+  const userPlayer = players.find((player) => player.uid === user?.uid);
+  const owner = players.find((player) => player.uid === ownEvent?.player_id);
 
   const displayTimestamp = ownEvent
     ? moment(ownEvent.timestamp).format("HH:mm:ss, D MMM YYYY")
     : null;
 
-  const background = player?.color;
+  const background = owner?.color;
   const tooltip =
     ownEvent !== undefined
-      ? `${player?.name} at ${displayTimestamp}`
+      ? `${owner?.name} at ${displayTimestamp}`
       : "Uncaught";
 
   return (
     <StatefulTooltip content={tooltip} placement="bottom">
-      <img
-        className={css({ width: "48px", height: "48px", background })}
-        src={data?.sprites.front_default}
-        alt={name}
-      />
+      <div
+        style={{
+          position: "relative",
+          display: "inline-flex",
+        }}
+      >
+        <img
+          className={css({ width: "48px", height: "48px", background })}
+          src={data?.sprites.front_default}
+          alt={name}
+        />
+        {seen && (
+          <div style={{ position: "absolute", left: 0, top: 0, zIndex: 1 }}>
+            <FaEye color={userPlayer?.color} />
+          </div>
+        )}
+      </div>
     </StatefulTooltip>
   );
 };
